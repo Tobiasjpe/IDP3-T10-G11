23973dd34876ae046bbc5ec04ef8d8daff28a15e
Tobiasjpe <Tjparryedwards@gmail.com>
2025-03-24 16:14:40 +0000

    Update IDP3_Control_System.m
    Added some optimal conditions for KD

--- IDP3_Control_System.m
+++ IDP3_Control_System.m
@@ -1,3 +1,17 @@
 clear
 
 % matlab file to accompany simulink for IDP3
+
+%UVI
+UVI = 9;
+
+%Humidity
+Humidity = 0.7;
+
+%Temperature
+BaskingTemp = 45;
+SummerDay = 31;
+SummerNight = 25;
+WinterDay = 32;
+WinterNight = 26.5;
+
